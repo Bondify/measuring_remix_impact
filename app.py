d49ce0b7fb8e47ea8982c96e392ff8cc65d19fc5
Santiago Toso <39338250+Bondify@users.noreply.github.com>
2020-07-15 12:38:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 import math
+from datetime import datetime
 
 dictionary = {
     'agency_to_community':['1 - Primarily internal','2','3','4','5 - Primarily community-facing'],
@@ -20,7 +21,8 @@ dictionary = {
 
 score = pd.DataFrame.from_dict(dictionary)
 
-wins = pd.read_csv("https://raw.githubusercontent.com/Bondify/measuring_remix_impact/master/data.csv")
+wins = pd.read_csv(r"C:\Users\santi\Google Drive\Master\Python\Dash\Github-Heroku\Measure Remix impact\data\data.csv",
+                   parse_dates = ['Win Date'])
 
 # Keep this out of source code repository - save in a file or a database
 VALID_USERNAME_PASSWORD_PAIRS = {
@@ -55,7 +57,7 @@ wins_scored.rename(columns={'score':'score_size_of_impact'}, inplace=True)
 wins_scored = wins_scored.loc[~wins_scored['Win Date'].isnull()]
 wins_scored = wins_scored.loc[~wins_scored['score_agency_to_community'].isnull()]
 wins_scored = wins_scored.loc[~wins_scored['score_size_of_impact'].isnull()]
-wins_scored['year'] = [i[-4:] for i in wins_scored['Win Date']]
+wins_scored['year'] = [i.year for i in wins_scored['Win Date']] #[i[-4:] for i in wins_scored['Win Date']]
 wins_scored['size'] = wins_scored.score_agency_to_community * wins_scored.score_size_of_impact
 wins_scored['id'] = wins_scored.score_agency_to_community.map(str) + '-' + wins_scored.score_size_of_impact.map(str)
 
@@ -83,177 +85,11 @@ df = df.append(df_aux)
     
     
 df = df.append(df_aux)
+df['hover_text'] = '<b>Account Name:</b> ' + df['Account Name'] + '<br>' + '<b>Date:</b> ' +df['Win Date'].apply(lambda x: x.date().strftime("%d %b %Y"))
 
 dark_blue = "#13264B"
 dark_grey = '#787E81'
 remix_blue = '#005BAA'
-
-traces = [
-    go.Scatter(
-        x = df.score_agency_to_community,
-        y = df.score_size_of_impact,
-        hovertemplate = '<b>%{hovertext}</b><extra></extra>',
-        hovertext = df['Win: Win Name'].unique(),
-        ids = df['Win: Win Name'].unique(),
-        marker = dict(
-            color = remix_blue,
-            opacity = 0.8,
-            size = df['size'],
-            sizemode = 'area',
-            sizeref = 0.032,
-            symbol = 'circle'
-        ),
-        showlegend = False,
-        mode = 'markers',
-        customdata = df['Win Description'],
-    )
-]
-
-annotation_family = "Raleway, sans-serif"
-annotation_size = 16
-axis_size = 14
-
-shapes = [
-    dict(# Center Vertical line
-        type="line",
-            x0=3,#2.5,
-            y0=.75,#-.25,
-            x1=3,#2.5,
-            y1=5.25,
-            line=dict(
-                color= 'rgb(0,0,0)',
-                width=2
-            )
-        ),
-    dict(# Middle Horizontal Line
-            type="line",
-            x0=0.75,
-            y0=3,#2.5,
-            x1=5.25,
-            y1=3,#2.5,
-            line=dict(
-                color = 'rgb(0,0,0)',
-                width=2
-            )
-    ),
-    dict(# Lower Horizontal Line
-            type="line",
-            x0=0.75,
-            y0=.75,
-            x1=5.25,
-            y1=.75,
-            line=dict(
-                color = 'rgb(0,0,0)',
-                width=1
-            )
-    ),
-    dict(# Upper Line Horizontal
-            type="line",
-            x0=0.75,
-            y0=5.25,
-            x1=5.25,
-            y1=5.25,
-            line=dict(
-                color = 'rgb(0,0,0)',
-                width=1
-            )
-    ),
-    dict(# Left Vertical Line
-        type="line",
-            x0=.75,#-.25,
-            y0=.75,
-            x1=.75,#-.25,
-            y1=5.25,
-            line=dict(
-                color= 'rgb(0,0,0)',
-                width=1
-            )
-        ),
-    dict(# Right Vertical Line
-        type="line",
-            x0=5.25,
-            y0=.75,
-            x1=5.25,
-            y1=5.25,
-            line=dict(
-                color= 'rgb(0,0,0)',
-                width=1
-            )
-        ),
-]
-
-#u"\u03A6"
-annotations=[
-    dict(x=1.25, y=.55, #xref="paper", #yref="paper", 
-         text="Agency centric to Community Centric "+ u"\u2192", showarrow=False, 
-             font=dict(family=annotation_family,
-                       size=annotation_size,
-                       color='rgb(135,135,135)')
-        ),
-    dict(x=0.65, y=1.5, #xref="paper", #yref="paper", 
-         text="Low to high impact " + u"\u2192", showarrow=False,
-         textangle=270,
-             font=dict(family=annotation_family,
-                       size=annotation_size,
-                       color='rgb(135,135,135)')
-        ),
-    
-        dict(x=1.75,
-        y=1.75,
-        xref="x",
-        yref="y",
-        text="Small Impact<br>+<br>Agency Centric",
-        showarrow=False,
-        font=dict(
-                family=annotation_family,
-                size=annotation_size,
-                color='rgb(135,135,135)') 
-            ),
-        dict(x=1.75,
-        y=4.25,
-        xref="x",
-        yref="y",
-        text="Large Impact<br>+<br>Agency Centric",
-        showarrow=False,
-        font=dict(
-                family=annotation_family,
-                size=annotation_size,
-                color='rgb(135,135,135)') 
-            ),
-    dict(x=4.25,
-        y=1.75,
-        xref="x",
-        yref="y",
-        text="Small Impact<br>+<br>Community Centric",
-        showarrow=False,
-        font=dict(
-                family=annotation_family,
-                size=annotation_size,
-                color='rgb(135,135,135)') 
-        ),
-    dict(x=4.25,
-        y=4.25,
-        xref="x",
-        yref="y",
-        text="Large Impact<br>+<br>Community Centric",
-        showarrow=False,
-        font=dict(
-                family=annotation_family,
-                size=annotation_size,
-                color='rgb(135,135,135)') 
-        )
-]
-
-layout = go.Layout(annotations = annotations, shapes = shapes,
-    showlegend=False, hovermode='closest', template='simple_white', height=650,
-    coloraxis_showscale=False, font=dict(family=annotation_family, size=annotation_size,),
-    margin = dict(l = 0, b = 10, t=10, r=0)
-                  )
-
-fig = go.Figure(data=traces, layout=layout)
-
-fig.update_xaxes(visible=False, range = [0.5,5.5])
-fig.update_yaxes(visible=False, range = [0.5,5.5])
 
 app.layout = html.Div(
     [
@@ -261,10 +97,27 @@ app.layout = html.Div(
                 style = {"font-family":"Raleway, sans-serif", "color":"#13264B", 'font-size':32, 'marginLeft':35, 'marginTop':35}
                 ),
         
-        html.P('The plot below highlights Remix wins by the size of their social impact for 2019 and 2020.',
+        html.Div([
+            html.Div([
+                html.P('The plot below highlights Remix wins by the size of their social impact for 2019 and 2020.',
               style = {'line-height': 20, 'font-family': "Raleway, sans-serif", "color":dark_blue, 'font-size':18, 'marginLeft':35},
-            ),
-
+            ),], style={"width": "70%",'float': 'left'}),
+            
+            html.Div([
+                    dcc.DatePickerRange(
+                            id = 'date_range',
+                            start_date = wins['Win Date'].min(),
+                            end_date=datetime.now(),
+                            display_format='MMM Do, YY',
+                            start_date_placeholder_text='Start date',
+                            end_date_placeholder_text='End date',
+                            #persistence = True,
+                            #persisted_props = ['start_date', 'end_date'],
+                            number_of_months_shown = 3,
+                            with_portal = True,
+                   )], style = {"width": "25%", "float": "left"})
+        ]),
+        
        html.Div([
            html.Div([
             dcc.Graph(id="graph", 
@@ -272,38 +125,205 @@ app.layout = html.Div(
                  config={'displayModeBar': False},
                  style={'marginBottom':10}
                  ), 
-            ], style={"width": "75%",'float': 'left'}
+            ], style={"width": "70%",'float': 'left'}
             ),
         
             html.Div([
-                html.Div(id='account_name', style = {'font-weight': 'bold', 'marginBottom': 10}),
+                # html.Div(id='account_name', style = {'font-weight': 'bold', 'marginBottom': 10}),
                 html.Div(id = 'win_description', style={'line-height': 25})
-                ], style={"width": "20%", "float": "right", "font-family":"Raleway, sans-serif",
+                ], style={"width": "25%", "float": "left", "font-family":"Raleway, sans-serif",
                        "padding":'50 10 0 10', 'marginTop': 25, 'marginRight':20,'display': 'inline-block',
                        #'marginBottom': 50, 
                        },
             ),
-           
            ] ),
     ],# style = {}
 )
 
 @app.callback(
         dash.dependencies.Output('graph', 'figure'),
-        [dash.dependencies.Input('graph', 'hoverData')]
+        [dash.dependencies.Input('graph', 'hoverData'),
+         dash.dependencies.Input('date_range', 'start_date'),
+         dash.dependencies.Input('date_range', 'end_date')]
         )
 
-def show_fig(hover_data):
+def show_fig(hover_data, start_date, end_date):
+    df_filtered = df.loc[(df['Win Date']>start_date)& (df['Win Date']<end_date),:]
+    traces = [
+    go.Scatter(
+        x = df_filtered.score_agency_to_community,
+        y = df_filtered.score_size_of_impact,
+        
+        hovertext = df_filtered['Win: Win Name'], #df['hover_text'].unique(),
+        text = df_filtered['hover_text'],
+        hovertemplate = 
+        #'<br><b>Account Name</b>: %{name}<br>'+
+        #'<b>%{text}</b><br>' + 
+        '%{text}<br>' + 
+        '<br><b>Win Name</b>: %{hovertext}<br>',
+        
+        ids = df_filtered['Win: Win Name'],
+        
+        marker = dict(
+            color = remix_blue,
+            opacity = 0.8,
+            size = df_filtered['size'],
+            sizemode = 'area',
+            sizeref = 0.032,
+            symbol = 'circle'
+        ),
+        showlegend = False,
+        mode = 'markers',
+        customdata = df_filtered['Win Description'],
+    )]
+
+    annotation_family = "Raleway, sans-serif"
+    annotation_size = 16
+    axis_size = 14
+    
+    shapes = [
+        dict(# Center Vertical line
+            type="line",
+                x0=3,#2.5,
+                y0=.75,#-.25,
+                x1=3,#2.5,
+                y1=5.25,
+                line=dict(
+                    color= 'rgb(0,0,0)',
+                    width=2
+                )
+            ),
+        dict(# Middle Horizontal Line
+                type="line",
+                x0=0.75,
+                y0=3,#2.5,
+                x1=5.25,
+                y1=3,#2.5,
+                line=dict(
+                    color = 'rgb(0,0,0)',
+                    width=2
+                )
+        ),
+        dict(# Lower Horizontal Line
+                type="line",
+                x0=0.75,
+                y0=.75,
+                x1=5.25,
+                y1=.75,
+                line=dict(
+                    color = 'rgb(0,0,0)',
+                    width=1
+                )
+        ),
+        dict(# Upper Line Horizontal
+                type="line",
+                x0=0.75,
+                y0=5.25,
+                x1=5.25,
+                y1=5.25,
+                line=dict(
+                    color = 'rgb(0,0,0)',
+                    width=1
+                )
+        ),
+        dict(# Left Vertical Line
+            type="line",
+                x0=.75,#-.25,
+                y0=.75,
+                x1=.75,#-.25,
+                y1=5.25,
+                line=dict(
+                    color= 'rgb(0,0,0)',
+                    width=1
+                )
+            ),
+        dict(# Right Vertical Line
+            type="line",
+                x0=5.25,
+                y0=.75,
+                x1=5.25,
+                y1=5.25,
+                line=dict(
+                    color= 'rgb(0,0,0)',
+                    width=1
+                )
+            ),
+    ]
+    
+    #u"\u03A6"
+    annotations=[
+        dict(x=1.25, y=.55, #xref="paper", #yref="paper", 
+             text="Agency centric to Community Centric "+ u"\u2192", showarrow=False, 
+                 font=dict(family=annotation_family,
+                           size=annotation_size,
+                           color='rgb(135,135,135)')
+            ),
+        dict(x=0.65, y=1.5, #xref="paper", #yref="paper", 
+             text="Low to high impact " + u"\u2192", showarrow=False,
+             textangle=270,
+                 font=dict(family=annotation_family,
+                           size=annotation_size,
+                           color='rgb(135,135,135)')
+            ),
+        
+            dict(x=1.75,
+            y=1.75,
+            xref="x",
+            yref="y",
+            text="Small Impact<br>+<br>Agency Centric",
+            showarrow=False,
+            font=dict(
+                    family=annotation_family,
+                    size=annotation_size,
+                    color='rgb(135,135,135)') 
+                ),
+            dict(x=1.75,
+            y=4.25,
+            xref="x",
+            yref="y",
+            text="Large Impact<br>+<br>Agency Centric",
+            showarrow=False,
+            font=dict(
+                    family=annotation_family,
+                    size=annotation_size,
+                    color='rgb(135,135,135)') 
+                ),
+        dict(x=4.25,
+            y=1.75,
+            xref="x",
+            yref="y",
+            text="Small Impact<br>+<br>Community Centric",
+            showarrow=False,
+            font=dict(
+                    family=annotation_family,
+                    size=annotation_size,
+                    color='rgb(135,135,135)') 
+            ),
+        dict(x=4.25,
+            y=4.25,
+            xref="x",
+            yref="y",
+            text="Large Impact<br>+<br>Community Centric",
+            showarrow=False,
+            font=dict(
+                    family=annotation_family,
+                    size=annotation_size,
+                    color='rgb(135,135,135)') 
+            )
+    ]
+    
+    layout = go.Layout(annotations = annotations, shapes = shapes,
+        showlegend=False, hovermode='closest', template='simple_white', height=650,
+        coloraxis_showscale=False, font=dict(family=annotation_family, size=annotation_size,),
+        margin = dict(l = 0, b = 10, t=10, r=0)
+                      )
+    
+    fig = go.Figure(data=traces, layout=layout)
+    
+    fig.update_xaxes(visible=False, range = [0.5,5.5])
+    fig.update_yaxes(visible=False, range = [0.5,5.5])
     return fig
 
-@app.callback(
-        dash.dependencies.Output('account_name', 'children'),
-        [dash.dependencies.Input('graph', 'hoverData')]
-        )
-
-def account_name_div(hover_data):
-    description = hover_data['points'][0]['customdata']
-    return df.loc[df['Win Description']== description, 'Account Name']
 
 @app.callback(
         dash.dependencies.Output('win_description', 'children'),
@@ -316,3 +336,4 @@ def update_output_div(hover_data):
 
 if __name__ == '__main__':
     app.run_server(debug=False)
+    
